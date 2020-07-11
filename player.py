9dc36fc38ad0552934baa4890157ef224247794a
Andreas Ferstad <aoferstad@gmail.com>
2020-07-11 12:36:02 +0200

    add city and player file

---
+++ player.py
@@ -0,0 +1,9 @@
+# player
+
+class player:
+
+    def __init__(self):
+        """
+        input: unsplit prepared data, learning_method, and possibility to change default parameters
+        """
+        pass
